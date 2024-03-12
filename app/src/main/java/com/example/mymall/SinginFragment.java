49d82faf1cc2c665a4fa0e99a3d580a2427ece8d
mhrah <mhrahatulislam@gmail.com>
2024-03-12 10:04:33 +0600

    My commit 5:04

--- app/src/main/java/com/example/mymall/SinginFragment.java
+++ app/src/main/java/com/example/mymall/SinginFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SinginFragment extends Fragment {
     public SinginFragment() {
@@ -19,6 +24,17 @@ public class SinginFragment extends Fragment {
     private TextView dontHaveAnAccount;
     private FrameLayout parentFramelayout;
 
+    private EditText editText_email;
+    private EditText editText_password;
+
+    private TextView textViewforget_password;
+
+    private Button button_singup;
+
+    private ImageView button_close;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,6 +42,15 @@ public class SinginFragment extends Fragment {
         View view= inflater.inflate(R.layout.singin_fragment, container, false);
         dontHaveAnAccount=view.findViewById(R.id.btn_singup_donthaveAnAccount);
         parentFramelayout = requireActivity().findViewById(R.id.registration_framelayout);
+
+        editText_email= view.findViewById(R.id.email_filed);
+        editText_password=view.findViewById(R.id.password_filed);
+        textViewforget_password=view.findViewById(R.id.forgat_password);
+        button_singup= view.findViewById(R.id.btn_singin);
+        button_close = view.findViewById(R.id.btn_image);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         return view;
     }
     @Override
--- app/src/main/java/com/example/mymall/SingupFragment.java
+++ app/src/main/java/com/example/mymall/SingupFragment.java
@@ -224,4 +224,5 @@ public class SingupFragment extends Fragment {
 
     }
 
+
 }
